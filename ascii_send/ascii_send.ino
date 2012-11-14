804c41b0bb1fb6f5a1a1b02ee548572e4fbc7322
Myles Borins <myles.borins@gmail.com>
2012-11-13 22:55:52 -0800

    updated ascii_send.ino

--- ascii_send/ascii_send.ino
+++ ascii_send/ascii_send.ino
@@ -6,45 +6,23 @@
  */ 
 	 
 // Initial Pin Settings
-int switchPin = 2;
 int accxPin = 0;
 int accyPin = 1;
 int acczPin = 2;
-int forcePin = 5;
 
 // Set things up
-int switchState = 0;
-int forceState = 0;
 int accelX = 0;
 int accelY = 0;
 int accelZ = 0;
 
 void setup(){
-	// Set pins as inputs
-	pinMode(2, INPUT);   // digital sensor is on digital pin 2
-	
 	// Start serial like a gentleman
 	Serial.begin(57600);
 }
 
 void loop(){
-	pollSwitch();
-	delay(1);
 	pollAccelerometer();
-	delay(1);
-	pollForce();
 	delay(10);
-}
-
-void pollSwitch(){
-	int reading = digitalRead(switchPin);
-	
-	if(reading != switchState){
-		switchState = reading;
-		Serial.print('a');
-		Serial.print(' ');
-		Serial.println(switchState);
-	}
 }
 
 void pollAccelerometer(){
@@ -59,12 +37,4 @@ void pollAccelerometer(){
 	Serial.print(accelY);
 	Serial.print(' ');
 	Serial.println(accelZ);
-}
-
-void pollForce(){
-	forceState = analogRead(forcePin);
-	
-	Serial.print('c');
-	Serial.print(' ');
-	Serial.println(forceState);
 }
